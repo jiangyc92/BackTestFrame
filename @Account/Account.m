99bbb64d0015bae49cc293ad5470cad3bc319d72
Yuchen Jiang <Yuchen Jiang>
2016-08-14 20:33:54 +0800

    初次更新

--- @Account/Account.m
+++ @Account/Account.m
@@ -1,2 +1,16 @@
 classdef Account < handle
+    properties
+        StockPool
+        RemainedStocksToSell
+        RemainedStocksToBuy       % malloc when used
+        RemainedMaxDay
+    end
+    
+    methods
+        InitAccount(obj, btobj);
+        ChangePosition(obj, btobj, Tickers, Pcts, PriceField);
+        ChangeStockPool(obj, btobj, Tickers, Pcts, PriceField);
+        HandleRemainedStocks(obj, btobj);
+        FlushRemainedStocks(obj, btobj);
+    end
 end
